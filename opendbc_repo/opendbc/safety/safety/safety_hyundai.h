e228c8165392e703f6a072412a673bb5b253c0a2
ajouatom <ajouatom@gmail.com>
2025-11-16 15:44:30 +0900

    ff

--- opendbc_repo/opendbc/safety/safety/safety_hyundai.h
+++ opendbc_repo/opendbc/safety/safety/safety_hyundai.h
@@ -216,7 +216,7 @@ static bool hyundai_tx_hook(const CANPacket_t *to_send) {
     bool CF_VSM_DecCmdAct = GET_BIT(to_send, 31U);
 
     if ((CR_VSM_DecCmd != 0) || FCA_CmdAct || CF_VSM_DecCmdAct) {
-      tx = false;
+      //tx = false;
     }
   }
 
