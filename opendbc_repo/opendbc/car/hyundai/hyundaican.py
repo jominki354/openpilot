9d375599a58f50fd270e3fa3d2e488e69eea21c2
ajouatom <ajouatom@gmail.com>
2025-11-16 14:55:33 +0900

    ff

--- opendbc_repo/opendbc/car/hyundai/hyundaican.py
+++ opendbc_repo/opendbc/car/hyundai/hyundaican.py
@@ -212,7 +212,7 @@ def create_acc_commands_scc(packer, enabled, accel, jerk, idx, hud_control, set_
 
   if CS.fca11 is not None and use_fca:
     values = copy.copy(CS.fca11)
-    values["FCA_FailInfo"] = 0
+    values["FCA_Failinfo"] = 0
     fca11_dat = packer.make_can_msg("FCA11", 0, values)[1]
     values["CR_FCA_ChkSum"] = hyundai_checksum(fca11_dat[:7])
     commands.append(packer.make_can_msg("FCA11", 0, values))
